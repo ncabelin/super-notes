81b944a63fc13ee3f2c926b353c5252a89c14b96
Neptune Michael Cabelin <ncabelin@gmail.com>
2016-06-01 14:45:44 +0000

    functional update

--- app.js
+++ app.js
@@ -1,15 +1,94 @@
 var express = require('express'),
     app = express(),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    mongoose = require('mongoose');
 
-
+// APP CONFIG
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended:true}));
-
-app.get('/', function(req, res) {
-   res.send('Reached homepage'); 
+app.use(express.static('public'));
+app.use(methodOverride('_method'));
+mongoose.connect('mongodb://localhost/notes_app');
+var noteSchema = new mongoose.Schema({
+   question: String,
+   answer: String
 });
+var Note = mongoose.model('noteModel', noteSchema);
+
+// SHOW ALL ROUTE
+app.get('/', function(req, res) {
+    Note.find({}, function(err, data) {
+       if (err) {
+           console.log(err);
+       } else {
+           res.render('body', { notesData: data});
+       }
+    });
+});
+
+// CREATE ROUTE
+app.post('/save', function(req, res) {
+    var question = req.body.question,
+        answer = req.body.answer;
+
+    Note.create({
+      question: question,
+      answer: answer
+    }, function(err, note) {
+       if (err) {
+           console.log(err);
+       } else {
+           console.log('Saved ' + note);
+           res.redirect('/');
+       }
+    });
+});
+
+// EDIT ROUTE
+app.get('/edit/:id', function(req, res) {
+    var NoteId = req.params.id;
+    Note.findById(NoteId, function(err, noteData) {
+       if (err) {
+           console.log(err);
+       } else {
+           res.render('edit', { edit: noteData, id: NoteId });
+       }
+    });
+});
+
+// UPDATE ROUTE
+app.put('/:id', function(req, res) {
+    var question = req.body.question,
+        answer = req.body.answer,
+        id = req.params.id;
+    
+    Note.findByIdAndUpdate(id, { question: question, answer: answer }, function(err, data) {
+       if (err) {
+           console.log(err);
+       } else {
+           console.log('Updated :' + data);
+           res.redirect('/');
+       }
+    });
+});
+
+// DELETE ROUTE
+app.get('/delete/:id', function(req, res) {
+    var question = req.body.question,
+        answer = req.body.answer,
+        id = req.params.id;
+     
+   Note.findByIdAndRemove(id, { question: question, answer: answer }, function(err, data) {
+       if (err) {
+           console.log(err);
+       } else {
+           console.log('Deleted :' + data);
+           res.redirect('/');
+       }
+    }); 
+});
+
 
 app.listen(process.env.PORT, process.env.IP, function() {
    console.log('Supernotes server started..'); 
